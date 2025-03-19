29ecce0c2db30000a36370e57dc6ac618ea08bb7
Rockolongas <113271897+Rockolott@users.noreply.github.com>
2025-03-19 12:38:37 -0500

    cambios

---
+++ SISE/resources/js/components/ExampleComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>{{ mensaje }}</h1>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const mensaje = ref('Hola desde Vue 3 con Laravel!');
+  </script>
+  
