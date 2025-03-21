79ea8966bbb9c65defff939378ced8f2df216b20
Axl <113398589+Axlreb28@users.noreply.github.com>
2025-03-21 14:39:20 -0500

    sin sqlite

--- config/database.php
+++ config/database.php
@@ -19,6 +19,7 @@ return [
     'default' => env('DB_CONNECTION', 'mysql'),
 
 
+
     /*
     |--------------------------------------------------------------------------
     | Database Connections
@@ -32,16 +33,7 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-            'busy_timeout' => null,
-            'journal_mode' => null,
-            'synchronous' => null,
-        ],
+        
 
         'mysql' => [
             'driver' => 'mysql',
